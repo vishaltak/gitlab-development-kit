9e29da100e3a5fa19dcd5a3405b16c03a8407baa
Shinya Maeda <shinya@gitlab.com>
2017-10-27 16:17:36 +0900

    Omit prompt for runner conf file if ce or ee feeded

--- set_ip.rb
+++ set_ip.rb
@@ -23,16 +23,26 @@ ip = prompt.select("Choose ip", ipv4)
 # Overwrite files
 
 # host
-File.open("#{$gdk_root}/host", 'w') { |file| file.write(ip) }
+File.open("./host", 'w') { |file| file.write(ip) }
 
 # gitlab/config/gitlab.yml
-File.read("#{$gdk_root}/gitlab/config/gitlab.yml").tap do |content|
+File.read("./gitlab/config/gitlab.yml").tap do |content|
   new_content = content.gsub(/(^production:.*?gitlab:.*?host:)\s*([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|localhost)/m, '\1 '+ip)
-  File.open("#{$gdk_root}/gitlab/config/gitlab.yml", 'w') { |file| file.write(new_content) }
+  File.open("./gitlab/config/gitlab.yml", 'w') { |file| file.write(new_content) }
 end
 
 # ~/.gitlab-runner/config.toml
-runner_file = prompt.ask('Tell me your runner configuration file', default: ENV['HOME']+'/.gitlab-runner/config.toml')
+runner_file =
+  if ARGV[0] == 'ce'
+    ENV['HOME']+'/.gitlab-runner/config.toml'
+  elsif ARGV[0] == 'ee'
+    ENV['HOME']+'/.gitlab-runner/config-ee.toml'
+  else
+    prompt.ask('Tell me your runner configuration file', default: ENV['HOME']+'/.gitlab-runner/config.toml')
+  end
+
+puts "runner_file is #{runner_file}"
+
 # puts "runner_file: #{runner_file}"
 File.read(runner_file).tap do |content|
   new_content = content.gsub(/(url = "http:\/\/)([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}|localhost)(:\d+\/(ci|)")/, '\1'+ip+'\3')
