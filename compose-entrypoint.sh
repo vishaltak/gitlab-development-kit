3c9a48cfbd466a265139315d609770744020301f
Matija Čupić <matteeyah@gmail.com>
2017-11-10 17:16:38 +0100

    Symlink gitlab shell before running gdk

--- compose-entrypoint.sh
+++ compose-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ -f gitlab/.gdk-installed ]; then
-	make Procfile
-	gdk run
+	make symlink-gitlab-shell
+	make Procfile && gdk run
 else
 	gdk install && touch gitlab/.gdk-installed
 	pkill -9 -f node
