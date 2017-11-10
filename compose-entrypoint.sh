c12ddb29ed3fd46a5589be37f944c4bac2ffa0a9
Matija Čupić <matteeyah@gmail.com>
2017-11-11 00:10:10 +0100

    Chown the bind mounts to the gdk user

--- compose-entrypoint.sh
+++ compose-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# chown the bind mounts
+sudo chown gdk:gdk gitlab gitlab-workhorse go-gitlab-shell gitaly
+
 if [ -f gitlab/.gdk-installed ]; then
 	make symlink-gitlab-shell
 	make Procfile && gdk run
