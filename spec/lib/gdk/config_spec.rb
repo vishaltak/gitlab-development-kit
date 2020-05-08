18707672eeb0e7574d2dc72e0899c6690a75940d
Ash McKenzie <amckenzie@gitlab.com>
2020-05-08 22:52:20 +1000

    Support redis as a Ruby Service

--- spec/lib/gdk/config_spec.rb
+++ spec/lib/gdk/config_spec.rb
@@ -2862,4 +2862,12 @@ RSpec.describe GDK::Config do
 
     path
   end
+
+  describe 'redis' do
+    describe '#dir' do
+      it 'returns the redis directory' do
+        expect(config.redis.dir).to eq(Pathname.new('/home/git/gdk/redis'))
+      end
+    end
+  end
 end
