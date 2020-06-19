2971a0f827e98ec308dc41619d41b02274eaa305
Gabriel Mazetto <brodock@gmail.com>
2020-06-19 15:07:11 +0200

    Move run_env generation to Runit::Config

--- lib/gdk/config_settings.rb
+++ lib/gdk/config_settings.rb
@@ -89,23 +89,6 @@ module GDK
       yaml
     end
 
-    def dump_run_env!
-      run_env = <<~RUN_ENV
-        export host=#{config.hostname}
-        export port=#{config.port}
-        export relative_url_root=#{config.relative_url_root}
-      RUN_ENV
-
-      if config.tracer.jaeger?
-        run_env += <<~RUN_ENV
-          export GITLAB_TRACING='opentracing://jaeger?http_endpoint=http%3A%2F%2Flocalhost%3A14268%2Fapi%2Ftraces&sampler=const&sampler_param=1'
-          export GITLAB_TRACING_URL='http://localhost:16686/search?service={{ service }}&tags=%7B"correlation_id"%3A"{{ correlation_id }}"%7D'
-        RUN_ENV
-      end
-
-      run_env
-    end
-
     def cmd!(cmd)
       # Passing an array to IO.popen guards against sh -c.
       # https://gitlab.com/gitlab-org/gitlab/blob/master/doc/development/shell_commands.md#bypass-the-shell-by-splitting-commands-into-separate-tokens
--- lib/runit/config.rb
+++ lib/runit/config.rb
@@ -29,7 +29,7 @@ module Runit
     end
 
     def run_env
-      @run_env ||= GDK::Config.new.dump_run_env!
+      @run_env ||= generate_run_env
     end
 
     def render(services: services_from_procfile)
@@ -64,6 +64,23 @@ module Runit
     end
 
     private
+
+    def generate_run_env
+      run_env = <<~RUN_ENV
+        export host=#{GDK.config.hostname}
+        export port=#{GDK.config.port}
+        export relative_url_root=#{GDK.config.relative_url_root}
+      RUN_ENV
+
+      if GDK.config.tracer.jaeger?
+        run_env += <<~RUN_ENV
+          export GITLAB_TRACING='opentracing://jaeger?http_endpoint=http%3A%2F%2Flocalhost%3A14268%2Fapi%2Ftraces&sampler=const&sampler_param=1'
+          export GITLAB_TRACING_URL='http://localhost:16686/search?service={{ service }}&tags=%7B"correlation_id"%3A"{{ correlation_id }}"%7D'
+        RUN_ENV
+      end
+
+      run_env
+    end
 
     def services_from_procfile
       File.read('Procfile').lines.map do |line|
