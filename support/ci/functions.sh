451fbf6e7d08bb025ed55090d1a49ebb083148db
Ash McKenzie <amckenzie@gitlab.com>
2020-11-09 11:32:45 +1100

    Reduce CI verify functions to minimum

--- support/ci/functions.sh
+++ support/ci/functions.sh
@@ -23,7 +23,6 @@ checkout() {
 
 install() {
   cd "${GDK_CHECKOUT_PATH}" || exit
-  netstat -lpt
   echo "> Installing GDK.."
   gdk install shallow_clone=true
   support/set-gitlab-upstream
@@ -31,7 +30,6 @@ install() {
 
 update() {
   cd "${GDK_CHECKOUT_PATH}" || exit
-  netstat -lpt
   echo "> Updating GDK.."
   # we use `make update` instead of `gdk update` to ensure the working directory
   # is not reset to master.
@@ -42,15 +40,14 @@ update() {
 
 start() {
   cd "${GDK_CHECKOUT_PATH}" || exit
-  killall node || true
   echo "> Starting up GDK.."
   gdk start
 }
 
 restart() {
   cd "${GDK_CHECKOUT_PATH}" || exit
-  gdk stop || true
-  gdk start
+  echo "> Restarting GDK.."
+  gdk restart
 }
 
 doctor() {
