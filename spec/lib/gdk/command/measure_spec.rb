e45e0acde582bfac70bd8a2fd9455bbcd6d96f04
Ash McKenzie <amckenzie@gitlab.com>
2021-01-15 12:35:36 +1100

    Remove unnecessary contexts

--- spec/lib/gdk/command/measure_spec.rb
+++ spec/lib/gdk/command/measure_spec.rb
@@ -13,74 +13,70 @@ RSpec.describe GDK::Command::Measure do
   end
 
   describe '#run' do
-    context 'with some requirements lacking' do
-      context 'when an empty urls is provided' do
-        let(:urls) { [] }
+    context 'when an empty urls is provided' do
+      let(:urls) { [] }
 
+      it 'aborts' do
+        expected_error = 'ERROR: Please add URL(s) as an argument (e.g. http://localhost:3000/explore, /explore or https://gitlab.com/explore)'
+
+        expect { subject.run }.to raise_error(expected_error).and output("#{expected_error}\n").to_stderr
+      end
+    end
+
+    context "when Docker isn't installed or running " do
+      let(:urls) { urls_default }
+
+      it 'aborts' do
+        stub_docker_check(success: false)
+
+        expected_error = 'ERROR: Docker is not installed or running!'
+
+        expect { subject.run }.to raise_error(expected_error).and output("#{expected_error}\n").to_stderr
+      end
+    end
+
+    context "when GDK isn't running " do
+      let(:urls) { urls_default }
+
+      context 'when GDK is not running' do
         it 'aborts' do
-          expected_error = 'ERROR: Please add URL(s) as an argument (e.g. http://localhost:3000/explore, /explore or https://gitlab.com/explore)'
+          expected_error = 'ERROR: GDK is not running locally on http://127.0.0.1:3000!'
 
           expect { subject.run }.to raise_error(expected_error).and output("#{expected_error}\n").to_stderr
         end
       end
 
-      context "when Docker isn't installed or running " do
-        let(:urls) { urls_default }
-
+      context 'when GDK is not ready' do
         it 'aborts' do
-          stub_docker_check(success: false)
+          stub_gdk_check(http_code: 502)
 
-          expected_error = 'ERROR: Docker is not installed or running!'
+          expected_error = 'ERROR: GDK is not running locally on http://127.0.0.1:3000!'
 
           expect { subject.run }.to raise_error(expected_error).and output("#{expected_error}\n").to_stderr
-        end
-      end
-
-      context "when GDK isn't running " do
-        let(:urls) { urls_default }
-
-        context 'when GDK is not running' do
-          it 'aborts' do
-            expected_error = 'ERROR: GDK is not running locally on http://127.0.0.1:3000!'
-
-            expect { subject.run }.to raise_error(expected_error).and output("#{expected_error}\n").to_stderr
-          end
-        end
-
-        context 'when GDK is not ready' do
-          it 'aborts' do
-            stub_gdk_check(http_code: 502)
-
-            expected_error = 'ERROR: GDK is not running locally on http://127.0.0.1:3000!'
-
-            expect { subject.run }.to raise_error(expected_error).and output("#{expected_error}\n").to_stderr
-          end
         end
       end
     end
 
-    context 'with all requirements present' do
-      let(:urls) { urls_default }
+    let(:urls) { urls_default }
 
-      it 'runs sitespeed via Docker', :hide_stdout do
-        branch_name = 'some-branch-name'
+    it 'runs sitespeed via Docker', :hide_stdout do
+      branch_name = 'some-branch-name'
 
-        stub_docker_check(success: true)
-        stub_gdk_check(http_code: 200)
-        stub_git_rev_parse(branch_name: branch_name)
+      stub_docker_check(success: true)
+      stub_gdk_check(http_code: 200)
+      stub_git_rev_parse(branch_name: branch_name)
 
-        freeze_time do
-          current_time = Time.now
-          current_time_formatted = current_time.strftime('%F-%H-%M-%S')
+      freeze_time do
+        current_time = Time.now
+        current_time_formatted = current_time.strftime('%F-%H-%M-%S')
 
-          shellout_docker_run_double = double('Shellout', stream: '')
-          allow(Shellout).to receive(:new).with(%[docker run --cap-add=NET_ADMIN --shm-size 2g --rm -v "$(pwd):/sitespeed.io" sitespeedio/sitespeed.io:15.9.0 -b chrome -n 4 -c cable --cookie perf_bar_enabled=false --outputFolder sitespeed-result/some-branch-name_#{current_time_formatted} http://host.docker.internal:3000/explore]).and_return(shellout_docker_run_double)
+        shellout_docker_run_double = double('Shellout', stream: '')
+        allow(Shellout).to receive(:new).with(%[docker run --cap-add=NET_ADMIN --shm-size 2g --rm -v "$(pwd):/sitespeed.io" sitespeedio/sitespeed.io:15.9.0 -b chrome -n 4 -c cable --cookie perf_bar_enabled=false --outputFolder sitespeed-result/some-branch-name_#{current_time_formatted} http://host.docker.internal:3000/explore]).and_return(shellout_docker_run_double)
 
-          shellout_open_double = double('Shellout', run: true)
-          allow(Shellout).to receive(:new).with("open ./sitespeed-result/#{branch_name}_#{current_time_formatted}/index.html").and_return(shellout_open_double)
+        shellout_open_double = double('Shellout', run: true)
+        allow(Shellout).to receive(:new).with("open ./sitespeed-result/#{branch_name}_#{current_time_formatted}/index.html").and_return(shellout_open_double)
 
-          subject.run
-        end
+        subject.run
       end
     end
   end
