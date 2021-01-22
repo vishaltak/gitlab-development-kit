576e02f509b487d01ad3f0e5faffae9a24263a79
Ash McKenzie <amckenzie@gitlab.com>
2021-01-22 22:25:30 +1100

    Refactor HTTPHelper stub to be more flexible

--- spec/lib/gdk/http_helper_spec.rb
+++ spec/lib/gdk/http_helper_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 
 RSpec.describe GDK::HTTPHelper do
   let(:uri) { GDK.config.__uri }
+  let(:path) { uri.path.empty? ? '/' : uri.path }
 
   describe '.new' do
     context 'when provided URI is not an instance of URI class' do
@@ -24,7 +25,7 @@ RSpec.describe GDK::HTTPHelper do
 
     context 'when URI is down because it returns a non successful HTTP code' do
       it 'returns false' do
-        stub_http_get(http_code: 502)
+        stub_http_get_with_code(502)
 
         expect(subject.up?).to be(false)
       end
@@ -32,20 +33,23 @@ RSpec.describe GDK::HTTPHelper do
 
     context 'when URI is up' do
       it 'returns true' do
-        stub_http_get(http_code: 200)
+        stub_http_get_with_code(200)
 
         expect(subject.up?).to be(true)
       end
     end
   end
 
-  def stub_http_get(http_code:)
-    uri = GDK.config.__uri
-    path = uri.path.empty? ? '/' : uri.path
-
+  def stub_http_get
     http_client_double = Net::HTTP.new(uri.host, uri.port)
     allow(Net::HTTP).to receive(:new).with(uri.host, uri.port).and_return(http_client_double)
     allow(http_client_double).to receive(:start).and_yield(http_client_double)
+
+    http_client_double
+  end
+
+  def stub_http_get_with_code(http_code)
+    http_client_double = stub_http_get
 
     http_response_double = double('Net::HTTPResponse', code: http_code.to_s)
     allow(http_client_double).to receive(:get).with(path).and_return(http_response_double)
