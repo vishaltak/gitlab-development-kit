779b2d66280bcf8587d339481879d1131840eeb1
Ash McKenzie <amckenzie@gitlab.com>
2021-02-05 18:04:55 +1100

    Refactor ResetData to be more SRP focused

--- lib/gdk/command/reset_data.rb
+++ lib/gdk/command/reset_data.rb
@@ -5,40 +5,36 @@ module GDK
     # Handles `gdk reset-data` command execution
     class ResetData
       def run
+        GDK.remember!(GDK.root)
+        Runit.stop
+
+        unless backup_data
+          GDK::Output.error('Failed to backup data.')
+          GDK.display_help_message
+          return
+        end
+
         reset_data
       end
 
       private
 
       def reset_data
-        GDK.remember!(GDK.root)
-        Runit.stop
-
-        path = GDK.root.join('./support/backup-data')
-        sh = Shellout.new(path.to_s, chdir: GDK.root)
-        sh.run
-
-        unless sh.success?
-          GDK::Output.error('Failed to backup data.')
-          GDK.display_help_message
-          return
-        end
-
-        make_result = GDK.make
-
-        GDK::Output.puts
-
-        if make_result
+        if GDK.make
           GDK::Output.notice('Successfully reset data!')
           GDK.start([])
         else
           GDK::Output.error('Failed to reset data.')
           GDK.display_help_message
+          false
         end
+      end
 
-        GDK::Output.puts
-
-        make_result
+      def backup_data
+        path = GDK.root.join('./support/backup-data')
+        sh = Shellout.new(path.to_s, chdir: GDK.root)
+        sh.run
+        sh.success?
       end
     end
   end
