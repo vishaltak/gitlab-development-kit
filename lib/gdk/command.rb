4a847d60354f126f8d9dc3e72aa753020000b286
Ash McKenzie <amckenzie@gitlab.com>
2021-02-11 15:26:58 +1100

    Alpha sort GDK commands

--- lib/gdk/command.rb
+++ lib/gdk/command.rb
@@ -5,13 +5,13 @@ module GDK
     autoload :Config, 'gdk/command/config'
     autoload :DiffConfig, 'gdk/command/diff_config'
     autoload :Doctor, 'gdk/command/doctor'
+    autoload :Help, 'gdk/command/help'
     autoload :Install, 'gdk/command/install'
     autoload :Measure, 'gdk/command/measure'
-    autoload :Help, 'gdk/command/help'
     autoload :Reconfigure, 'gdk/command/reconfigure'
+    autoload :ResetData, 'gdk/command/reset_data'
     autoload :Run, 'gdk/command/run'
     autoload :Thin, 'gdk/command/thin'
     autoload :Update, 'gdk/command/update'
-    autoload :ResetData, 'gdk/command/reset_data'
   end
 end
