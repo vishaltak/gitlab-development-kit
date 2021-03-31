999dcdb1738e3b84d27d8328f12c382c9734b259
Ash McKenzie <amckenzie@gitlab.com>
2021-03-31 13:28:35 +1100

    Alpha sort diagnostic requires

--- lib/gdk/diagnostic.rb
+++ lib/gdk/diagnostic.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
 require_relative 'diagnostic/base'
-require_relative 'diagnostic/dependencies'
-require_relative 'diagnostic/version'
-require_relative 'diagnostic/status'
-require_relative 'diagnostic/pending_migrations'
 require_relative 'diagnostic/configuration'
+require_relative 'diagnostic/dependencies'
 require_relative 'diagnostic/geo'
-require_relative 'diagnostic/postgresql'
-require_relative 'diagnostic/ruby_gems'
-require_relative 'diagnostic/re2'
 require_relative 'diagnostic/golang'
+require_relative 'diagnostic/pending_migrations'
+require_relative 'diagnostic/postgresql'
+require_relative 'diagnostic/re2'
+require_relative 'diagnostic/ruby_gems'
 require_relative 'diagnostic/stale_services'
+require_relative 'diagnostic/status'
+require_relative 'diagnostic/version'
 
 module GDK
   module Diagnostic
