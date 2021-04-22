35c5056653c629e11d8b4fb764370c5da7d40969
Lukas Eipert <leipert@gitlab.com>
2021-04-22 14:02:33 +0200

    Improve gitpod workspace image build
    This is a major change to how we build the image, instead of basing our
    workspace image on gitpod/workspace-full we are now utilizing the much
    smaller gitpod/workspace-base which doesn't ship things like e.g. java.
    
    Furthermore we are now utilizing `asdf` and our `make bootstrap`
    installation. This reduces the image size immensely, hopefully leading
    to faster startup times. We went down from 5.7 GB => 2.9 GB.
    
    Additionally we are preparing the CI pipeline for scheduled builds so
    that we can update the gitpod workspace image more often.

---
+++ support/gitpod/bootstrap-gitpod-image.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+sudo apt-get update
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get install -y make git
+
+# Install GitLab Runner
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
+export GITLAB_RUNNER_DISABLE_SKEL=true
+sudo apt-get install gitlab-runner -y
+
+# Install GDK
+sudo mkdir -p /workspace/gitlab
+sudo chown -R gitpod:gitpod /workspace
+cd /workspace
+git clone https://gitlab.com/gitlab-org/gitlab-development-kit.git
+cd gitlab-development-kit
+make bootstrap
+source "$HOME/.asdf/asdf.sh"
+gdk install shallow_clone=true
+gdk stop
+mv gitlab/config/secrets.yml .
+rm -rf gitlab
+rm -rf tmp
+cp ./support/completions/gdk.bash "$HOME/.bashrc.d/90-gdk"
+cd /workspace
+mv gitlab-development-kit "$HOME/"
+
+# Set up a symlink in order to have our .tool-versions as defaults.
+# A symlink ensures that it'll work even after a gdk update
+ln -s /workspace/gitlab-development-kit/.tool-versions "$HOME/.tool-versions"
+
+# Cleanup apt caches
+sudo apt-get clean -y
+sudo apt-get autoremove -y
+sudo rm -rf /var/cache/apt/* /var/lib/apt/lists/*
+
+# Cleanup build caches
+sudo rm -rf "$HOME/gitlab-development-kit/gitaly/_build/deps/git/source"
+sudo rm -rf "$HOME/gitlab-development-kit/gitaly/_build/deps/libgit2/source"
+sudo rm -rf "$HOME/gitlab-development-kit/gitaly/_build/cache"
+sudo rm -rf "$HOME/.cache/"
+sudo rm -rf /tmp/*
+
+# Cleanup temporary build folder
+sudo rm -rf /workspace
