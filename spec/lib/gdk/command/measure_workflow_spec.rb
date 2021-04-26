eda29f544f1559324f6499ccd5fb997457537a7d
Ash McKenzie <amckenzie@gitlab.com>
2021-04-26 13:09:49 +1000

    Use Time.now so freeze_time() can work

--- spec/lib/gdk/command/measure_workflow_spec.rb
+++ spec/lib/gdk/command/measure_workflow_spec.rb
@@ -31,7 +31,6 @@ RSpec.describe GDK::Command::MeasureWorkflow do
     context 'when Docker and GDK are running' do
       let(:docker_running) { true }
       let(:branch_name) { 'some-branch-name' }
-      let(:report_file_path) { "sitespeed-result/#{branch_name}_#{Time.now.strftime('%F-%H-%M-%S')}" }
 
       before do
         stub_gdk_check(is_running: true)
@@ -40,6 +39,7 @@ RSpec.describe GDK::Command::MeasureWorkflow do
 
       it 'runs sitespeed via Docker for the given workflow(s)', :hide_stdout do
         freeze_time do
+          report_file_path = "sitespeed-result/#{branch_name}_#{Time.now.strftime('%F-%H-%M-%S')}"
           workflows = %w[repo_browser]
           shellout_docker_run_double = double('Shellout', stream: '', success?: true)
           expect(Shellout).to receive(:new).with(%(docker run --cap-add=NET_ADMIN --shm-size 2g --rm -v "$(pwd):/sitespeed.io" sitespeedio/sitespeed.io:16.8.1 -b chrome -n 4 -c cable --cookie perf_bar_enabled=false --cpu --outputFolder #{report_file_path} --multi --spa support/measure_scripts/repo_browser.js)).and_return(shellout_docker_run_double)
