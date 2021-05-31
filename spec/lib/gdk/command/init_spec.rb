a5463f850ea9b2f898397cef07b69aba867687b6
Ash McKenzie <amckenzie@gitlab.com>
2021-05-31 15:41:16 +1000

    Break apart tests into single assertions

--- spec/lib/gdk/command/init_spec.rb
+++ spec/lib/gdk/command/init_spec.rb
@@ -11,45 +11,62 @@ RSpec.describe GDK::Command::Init do
 
   describe '#run' do
     context 'when asking for help' do
+      it 'returns true', :hide_output do
+        expect(subject.run(['--help'])).to be(true)
+      end
+
       it 'displays help and exits successfully' do
-        result = nil
-        expect { result = subject.run(['--help']) }.to output("#{deprecation_msg}Usage: gdk init [DIR]\n").to_stdout
-        expect(result).to be(true)
+        expect { subject.run(['--help']) }.to output("#{deprecation_msg}Usage: gdk init [DIR]\n").to_stdout
       end
     end
 
     context 'when init fails' do
       context 'because the nominated directory is invalid' do
-        it 'returns an error message' do
-          result = nil
-          bad_new_directory = '-bad'
-          stub_clone_gdk(bad_new_directory)
+        let(:bad_new_directory) { '-bad' }
 
-          expect { result = subject.run([bad_new_directory]) }.to output(deprecation_msg).to_stdout.and output("ERROR: The GDK directory cannot start with a dash.\n").to_stderr
-          expect(result).to be(false)
+        before do
+          stub_clone_gdk(bad_new_directory)
+        end
+
+        it 'returns false', :hide_stdout do
+          expect(subject.run([bad_new_directory])).to be(false)
+        end
+
+        it 'returns an error message' do
+          expect { subject.run([bad_new_directory]) }.to output(deprecation_msg).to_stdout.and output("ERROR: The GDK directory cannot start with a dash.\n").to_stderr
         end
       end
 
       context 'because there was an issue with the git clone' do
-        it 'returns an error message' do
-          result = nil
-          new_directory = 'gdk'
-          stub_clone_gdk(new_directory, success: false)
+        let(:new_directory) { 'gdk' }
 
-          expect { result = subject.run([new_directory]) }.to output(deprecation_msg).to_stdout.and output("ERROR: An error occurred while attempting to git clone the GDK into '#{new_directory}'.\n").to_stderr
-          expect(result).to be(false)
+        before do
+          stub_clone_gdk(new_directory, success: false)
+        end
+
+        it 'returns false', :hide_output do
+          expect(subject.run([new_directory])).to be(false)
+        end
+
+        it 'returns an error message' do
+          expect { subject.run([new_directory]) }.to output(deprecation_msg).to_stdout.and output("ERROR: An error occurred while attempting to git clone the GDK into '#{new_directory}'.\n").to_stderr
         end
       end
     end
 
     context 'when init succeeds' do
-      it 'returns no error message' do
-        result = nil
-        new_directory = 'gdk'
-        stub_clone_gdk(new_directory)
+      let(:new_directory) { 'gdk' }
 
-        expect { result = subject.run([new_directory]) }.to output("#{deprecation_msg}Successfully git cloned the GDK into '#{new_directory}'.\n").to_stdout
-        expect(result).to be(true)
+      before do
+        stub_clone_gdk(new_directory)
+      end
+
+      it 'returns true', :hide_stdout do
+        expect(subject.run([new_directory])).to be(true)
+      end
+
+      it 'returns no error message' do
+        expect { subject.run([new_directory]) }.to output("#{deprecation_msg}Successfully git cloned the GDK into '#{new_directory}'.\n").to_stdout
       end
     end
   end
