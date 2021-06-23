8b5453f9328d4b6af30283d3a354fb6522ad01c8
Rémy Coutable <remy@rymai.me>
2021-06-23 18:23:26 +0200

    Use the gitlab-dangerfiles gem
    Signed-off-by: Rémy Coutable <remy@rymai.me>

---
+++ tooling/danger/project_helper.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Tooling
+  module Danger
+    module ProjectHelper
+      CI_ONLY_RULES ||= %w[
+        roulette
+      ].freeze
+
+      def rule_names
+        helper.ci? ? CI_ONLY_RULES : []
+      end
+
+      # First-match win, so be sure to put more specific regex at the top...
+      CATEGORIES = {
+        %r{\Adoc/.*(\.(md|png|gif|jpg))\z} => :docs,
+        %r{\A(CONTRIBUTING|LICENSE|MAINTENANCE|PHILOSOPHY|PROCESS|README)(\.md)?\z} => :docs,
+
+        %r{.*} => nil
+      }.freeze
+
+      def changes_by_category
+        helper.changes_by_category(CATEGORIES)
+      end
+
+      def project_name
+        'gitlab-development-kit'
+      end
+    end
+  end
+end
