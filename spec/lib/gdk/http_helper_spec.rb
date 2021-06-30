6210aaacaa37e06dc77a844827b57a8ebaabd8c5
Ash McKenzie <amckenzie@gitlab.com>
2021-06-30 19:00:28 +1000

    New HTTPHelper head_up? and get_up?

--- spec/lib/gdk/http_helper_spec.rb
+++ spec/lib/gdk/http_helper_spec.rb
@@ -18,22 +18,30 @@ RSpec.describe GDK::HTTPHelper do
   subject { described_class.new(uri) }
 
   describe '#up?' do
+    it 'calls #get_up?' do
+      expect(subject).to receive(:get_up?)
+
+      subject.up?
+    end
+  end
+
+  describe '#get_up?' do
     context 'when URI is down because its not running' do
       it 'returns false' do
-        http_client_double = stub_http_get
+        http_client_double = stub_http
         allow(http_client_double).to receive(:get).with(path).and_raise(Errno::ECONNREFUSED)
 
-        expect(subject.up?).to be(false)
+        expect(subject.get_up?).to be(false)
         expect(subject.last_response_reason).to eq('Connection refused')
       end
     end
 
     context 'when URI is times out' do
       it 'returns false' do
-        http_client_double = stub_http_get
+        http_client_double = stub_http
         allow(http_client_double).to receive(:get).with(path).and_raise(Net::ReadTimeout)
 
-        expect(subject.up?).to be(false)
+        expect(subject.get_up?).to be(false)
         expect(subject.last_response_reason).to eq('Request timed out')
       end
     end
@@ -42,7 +50,7 @@ RSpec.describe GDK::HTTPHelper do
       it 'returns false' do
         stub_http_get_with_code(502)
 
-        expect(subject.up?).to be(false)
+        expect(subject.get_up?).to be(false)
         expect(subject.last_response_reason).to eq('502 Bad Gateway')
       end
     end
@@ -52,11 +60,11 @@ RSpec.describe GDK::HTTPHelper do
 
       it 'returns true' do
         stub_http_get_with_code(502)
-        expect(helper.up?).to be(false)
+        expect(helper.get_up?).to be(false)
         expect(helper.last_response_reason).to eq('502 Bad Gateway')
 
         stub_http_get_with_code(200)
-        expect(helper.up?).to be(true)
+        expect(helper.get_up?).to be(true)
         expect(helper.last_response_reason).to eq('200 OK')
       end
     end
@@ -65,13 +73,67 @@ RSpec.describe GDK::HTTPHelper do
       it 'returns true' do
         stub_http_get_with_code(200)
 
-        expect(subject.up?).to be(true)
+        expect(subject.get_up?).to be(true)
         expect(subject.last_response_reason).to eq('200 OK')
       end
     end
   end
 
-  def stub_http_get
+  describe '#head_up?' do
+    context 'when URI is down because its not running' do
+      it 'returns false' do
+        http_client_double = stub_http
+        allow(http_client_double).to receive(:head).with(path).and_raise(Errno::ECONNREFUSED)
+
+        expect(subject.head_up?).to be(false)
+        expect(subject.last_response_reason).to eq('Connection refused')
+      end
+    end
+
+    context 'when URI is times out' do
+      it 'returns false' do
+        http_client_double = stub_http
+        allow(http_client_double).to receive(:head).with(path).and_raise(Net::ReadTimeout)
+
+        expect(subject.head_up?).to be(false)
+        expect(subject.last_response_reason).to eq('Request timed out')
+      end
+    end
+
+    context 'when URI is down because it returns a non successful HTTP code' do
+      it 'returns false' do
+        stub_http_head_with_code(502)
+
+        expect(subject.head_up?).to be(false)
+        expect(subject.last_response_reason).to eq('502 Bad Gateway')
+      end
+    end
+
+    context 'when URI is down initially, but then comes up' do
+      subject(:helper) { described_class.new(uri, cache_response: false) }
+
+      it 'returns true' do
+        stub_http_head_with_code(502)
+        expect(helper.head_up?).to be(false)
+        expect(helper.last_response_reason).to eq('502 Bad Gateway')
+
+        stub_http_head_with_code(200)
+        expect(helper.head_up?).to be(true)
+        expect(helper.last_response_reason).to eq('200 OK')
+      end
+    end
+
+    context 'when URI is up' do
+      it 'returns true' do
+        stub_http_head_with_code(200)
+
+        expect(subject.head_up?).to be(true)
+        expect(subject.last_response_reason).to eq('200 OK')
+      end
+    end
+  end
+
+  def stub_http
     http_client_double = Net::HTTP.new(uri.host, uri.port)
     allow(Net::HTTP).to receive(:new).with(uri.host, uri.port).and_return(http_client_double)
     allow(http_client_double).to receive(:start).and_yield(http_client_double)
@@ -80,9 +142,16 @@ RSpec.describe GDK::HTTPHelper do
   end
 
   def stub_http_get_with_code(http_code)
-    http_client_double = stub_http_get
+    http_client_double = stub_http
 
     http_response_double = double('Net::HTTPResponse', code: http_code.to_s, message: Rack::Utils::HTTP_STATUS_CODES[http_code])
     allow(http_client_double).to receive(:get).with(path).and_return(http_response_double)
+  end
+
+  def stub_http_head_with_code(http_code)
+    http_client_double = stub_http
+
+    http_response_double = double('Net::HTTPResponse', code: http_code.to_s, message: Rack::Utils::HTTP_STATUS_CODES[http_code])
+    allow(http_client_double).to receive(:head).with(path).and_return(http_response_double)
   end
 end
