1f671f7a251d115db12fb3296251d84958d20f8b
Ash McKenzie <amckenzie@gitlab.com>
2021-07-27 15:23:20 +1000

    Also ensure .gitlab_shell_secret files exist

--- lib/gdk/diagnostic/gitlab.rb
+++ lib/gdk/diagnostic/gitlab.rb
@@ -10,45 +10,98 @@ module GDK
       end
 
       def success?
-        gitlab_shell_secret_matches?
+        gitlab_shell_secret_diagnostic.success?
       end
 
       def detail
         return if success?
 
-        <<~SECRET_MISMATCH_MSG
-          The gitlab-shell secret files need to match but they don't:
-
-          #{gitlab_shell_secret_in_gitlab}
-          #{'-' * gitlab_shell_secret_in_gitlab.to_s.length}
-          #{gitlab_shell_secret_in_gitlab_contents}
-
-          #{gitlab_shell_secret_in_gitlab_shell}
-          #{'-' * gitlab_shell_secret_in_gitlab_shell.to_s.length}
-          #{gitlab_shell_secret_in_gitlab_shell_contents}
-        SECRET_MISMATCH_MSG
+        gitlab_shell_secret_diagnostic.detail
       end
 
       private
 
-      def gitlab_shell_secret_matches?
-        gitlab_shell_secret_in_gitlab_contents == gitlab_shell_secret_in_gitlab_shell_contents
+      def gitlab_shell_secret_diagnostic
+        @gitlab_shell_secret_diagnostic ||= GitlabShellSecretDiagnostic.new(config)
       end
 
-      def gitlab_shell_secret_in_gitlab
-        config.gitlab.dir.join('.gitlab_shell_secret')
-      end
+      class GitlabShellSecretDiagnostic
+        GITLAB_SHELL_SECRET_FILE = '.gitlab_shell_secret'
 
-      def gitlab_shell_secret_in_gitlab_contents
-        @gitlab_shell_secret_in_gitlab_contents ||= gitlab_shell_secret_in_gitlab.read.chomp
-      end
+        def initialize(config)
+          @config = config
+        end
 
-      def gitlab_shell_secret_in_gitlab_shell
-        config.gitlab_shell.dir.join('.gitlab_shell_secret')
-      end
+        def success?
+          both_files_exist? && contents_match?
+        end
 
-      def gitlab_shell_secret_in_gitlab_shell_contents
-        @gitlab_shell_secret_in_gitlab_shell_contents ||= gitlab_shell_secret_in_gitlab_shell.read.chomp
+        def detail
+          return if success?
+
+          if !both_files_exist?
+            file_doesnt_exist_detail + solution_detail
+          elsif !contents_match?
+            contents_match_detail + solution_detail
+          end
+        end
+
+        private
+
+        attr_reader :config
+
+        def solution_detail
+          <<~SOLUTION_MESSAGE
+
+            The typical solution is to run 'gdk reconfigure'
+          SOLUTION_MESSAGE
+        end
+
+        def both_files_exist?
+          gitlab_shell_secret_in_gitlab.exist? && gitlab_shell_secret_in_gitlab_shell.exist?
+        end
+
+        def file_doesnt_exist_detail
+          output = ["The folllowing #{GITLAB_SHELL_SECRET_FILE} files don't exist but need to:", '']
+          output << "  #{gitlab_shell_secret_in_gitlab}" unless gitlab_shell_secret_in_gitlab.exist?
+          output << "  #{gitlab_shell_secret_in_gitlab_shell}" unless gitlab_shell_secret_in_gitlab_shell.exist?
+
+          "#{output.join("\n")}\n"
+        end
+
+        def contents_match?
+          gitlab_shell_secret_in_gitlab_contents == gitlab_shell_secret_in_gitlab_shell_contents
+        end
+
+        def contents_match_detail
+          <<~CONTENT_MISMATCH_MESSSGE
+            The gitlab-shell secret files need to match but they don't:
+
+            #{gitlab_shell_secret_in_gitlab}
+            #{'-' * gitlab_shell_secret_in_gitlab.to_s.length}
+            #{gitlab_shell_secret_in_gitlab_contents}
+
+            #{gitlab_shell_secret_in_gitlab_shell}
+            #{'-' * gitlab_shell_secret_in_gitlab_shell.to_s.length}
+            #{gitlab_shell_secret_in_gitlab_shell_contents}
+          CONTENT_MISMATCH_MESSSGE
+        end
+
+        def gitlab_shell_secret_in_gitlab
+          config.gitlab.dir.join(GITLAB_SHELL_SECRET_FILE)
+        end
+
+        def gitlab_shell_secret_in_gitlab_contents
+          @gitlab_shell_secret_in_gitlab_contents ||= gitlab_shell_secret_in_gitlab.read.chomp
+        end
+
+        def gitlab_shell_secret_in_gitlab_shell
+          config.gitlab_shell.dir.join(GITLAB_SHELL_SECRET_FILE)
+        end
+
+        def gitlab_shell_secret_in_gitlab_shell_contents
+          @gitlab_shell_secret_in_gitlab_shell_contents ||= gitlab_shell_secret_in_gitlab_shell.read.chomp
+        end
       end
     end
   end
--- spec/lib/gdk/diagnostic/gitlab_spec.rb
+++ spec/lib/gdk/diagnostic/gitlab_spec.rb
@@ -10,52 +10,130 @@ RSpec.describe GDK::Diagnostic::Gitlab do
   end
 
   describe '#success?' do
-    before do
-      stub_content('gitlab', 'abc')
-    end
+    context 'for .gitlab_shell_secret existence' do
+      before do
+        stub_existence('gitlab', 'gitlab', false)
+      end
 
-    context "when both .gitlab_shell_secret files don't match" do
-      it 'returns false' do
-        stub_content('gitlab_shell', 'def')
+      context "when both .gitlab_shell_secret files don't not exist" do
+        it 'returns false' do
+          stub_existence('gitlab_shell', 'gitlab-shell', false)
 
-        expect(subject.success?).to be_falsey
+          expect(subject.success?).to be_falsey
+        end
+      end
+
+      context 'when only the gitlab-shell .gitlab_shell_secret file exists' do
+        it 'returns false' do
+          stub_existence('gitlab_shell', 'gitlab-shell', true)
+
+          expect(subject.success?).to be_falsey
+        end
+      end
+
+      context 'when both .gitlab_shell_secret files exist' do
+        it 'returns true' do
+          stub_existence('gitlab', 'gitlab', true)
+          stub_existence('gitlab_shell', 'gitlab-shell', true)
+
+          expect(subject.success?).to be_truthy
+        end
       end
     end
 
-    context 'when both .gitlab_shell_secret files match' do
-      it 'returns true' do
-        stub_content('gitlab_shell', 'abc')
+    context 'for .gitlab_shell_secret contents' do
+      before do
+        stub_content('gitlab', 'gitlab', 'abc')
+      end
 
-        expect(subject.success?).to be_truthy
+      context "when both .gitlab_shell_secret files don't match" do
+        it 'returns false' do
+          stub_content('gitlab_shell', 'gitlab-shell', 'def')
+
+          expect(subject.success?).to be_falsey
+        end
+      end
+
+      context 'when both .gitlab_shell_secret files match' do
+        it 'returns true' do
+          stub_content('gitlab_shell', 'gitlab-shell', 'abc')
+
+          expect(subject.success?).to be_truthy
+        end
       end
     end
   end
 
   describe '#detail' do
-    before do
-      stub_content('gitlab', 'abc')
-    end
+    context 'for .gitlab_shell_secret existence' do
+      before do
+        stub_existence('gitlab', 'gitlab', false)
+      end
 
-    context "when both .gitlab_shell_secret files don't match" do
-      it 'returns detail content' do
-        stub_content('gitlab_shell', 'def')
+      context "when both .gitlab_shell_secret files don't exist" do
+        it 'returns detail content' do
+          stub_existence('gitlab_shell', 'gitlab-shell', false)
 
-        expect(subject.detail).to match(/The gitlab-shell secret files need to match but they don't/)
+          match = %r{  /home/git/gdk/gitlab/.gitlab_shell_secret\n  /home/git/gdk/gitlab-shell/.gitlab_shell_secret}
+
+          expect(subject.detail).to match(match)
+        end
+      end
+
+      context 'when only the gitlab-shell .gitlab_shell_secret file exists' do
+        it 'returns detail content' do
+          stub_existence('gitlab_shell', 'gitlab-shell', true)
+
+          match = %r{  /home/git/gdk/gitlab/.gitlab_shell_secret}
+
+          expect(subject.detail).to match(match)
+        end
+      end
+
+      context 'when both .gitlab_shell_secret files exist' do
+        it 'returns true' do
+          stub_existence('gitlab', 'gitlab', true)
+          stub_existence('gitlab_shell', 'gitlab-shell', true)
+
+          expect(subject.detail).to be_nil
+        end
       end
     end
 
-    context 'when both .gitlab_shell_secret files match' do
-      it 'returns nil' do
-        stub_content('gitlab_shell', 'abc')
+    context 'for .gitlab_shell_secret contents' do
+      before do
+        stub_content('gitlab', 'gitlab', 'abc')
+      end
 
-        expect(subject.detail).to be_nil
+      context "when both .gitlab_shell_secret files don't match" do
+        it 'returns detail content' do
+          stub_content('gitlab_shell', 'gitlab-shell', 'def')
+
+          expect(subject.detail).to match(/The gitlab-shell secret files need to match but they don't/)
+        end
+      end
+
+      context 'when both .gitlab_shell_secret files match' do
+        it 'returns nil' do
+          stub_content('gitlab_shell', 'gitlab-shell', 'abc')
+
+          expect(subject.detail).to be_nil
+        end
       end
     end
   end
 
-  def stub_content(project_dir, content)
-    dir = instance_double(Pathname, to_s: project_dir, read: content)
-    allow_any_instance_of(GDK::Config).to receive_message_chain(project_dir, :dir).and_return(dir)
-    allow(dir).to receive(:join).with('.gitlab_shell_secret').and_return(dir)
+  def stub_existence(key, project_dir, exist)
+    stub_file_with(key, project_dir, exist: exist)
+  end
+
+  def stub_content(key, project_dir, content)
+    stub_file_with(key, project_dir, content: content)
+  end
+
+  def stub_file_with(key, project_dir, content: 'abc', exist: true)
+    double = instance_double(Pathname, exist?: exist, to_s: "/home/git/gdk/#{project_dir}/.gitlab_shell_secret", read: content)
+    allow_any_instance_of(GDK::Config).to receive_message_chain(key, :dir).and_return(double)
+    allow(double).to receive(:join).with('.gitlab_shell_secret').and_return(double)
   end
 end
