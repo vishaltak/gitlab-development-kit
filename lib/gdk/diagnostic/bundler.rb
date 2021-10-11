977e7fb41eaff17b151bf2dc53ad5ce692b9a39e
Ash McKenzie <amckenzie@gitlab.com>
2021-10-11 14:35:26 +1100

    New Bundler diagnostic

---
+++ lib/gdk/diagnostic/bundler.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+module GDK
+  module Diagnostic
+    class Bundler < Base
+      TITLE = 'Bundler'
+
+      def diagnose
+        nil
+      end
+
+      def success?
+        gitlab_bundle_config.bundle_path_not_set? &&
+          gitaly_bundle_config.bundle_path_not_set?
+      end
+
+      def detail
+        return if success?
+
+        output = []
+        output << gitlab_bundle_config.warning_detail
+        output << gitaly_bundle_config.warning_detail
+        output.compact.join("\n")
+      end
+
+      private
+
+      class BundleConfig
+        def initialize(path)
+          @path = path
+        end
+
+        def bundle_path_not_set?
+          @bundle_path_not_set ||= bundle_path.include?('You have not configured a value for `PATH`')
+        end
+
+        def warning_detail
+          return if bundle_path_not_set?
+
+          <<~WARNING
+            #{path} appears to have BUNDLE_PATH configured
+            which can cause issues. For more detail,
+            visit https://gitlab.com/gitlab-org/gitlab-development-kit/-/issues/1315
+
+            #{bundle_path}
+          WARNING
+        end
+
+        private
+
+        attr_reader :path
+
+        def bundle_path
+          @bundle_path ||= Shellout.new('bundle config get PATH', chdir: path)
+                                   .execute(display_output: false)
+                                   .read_stdout
+        end
+      end
+
+      def gitlab_bundle_config
+        @gitlab_bundle_config ||= BundleConfig.new(config.gitlab.dir)
+      end
+
+      def gitaly_bundle_config
+        @gitaly_bundle_config ||= BundleConfig.new(config.gitaly.ruby_dir)
+      end
+    end
+  end
+end
---
+++ spec/lib/gdk/diagnostic/bundler_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe GDK::Diagnostic::Bundler do
+  let(:gitlab_dir) { Pathname.new('/home/git/gdk/gitlab') }
+  let(:gitaly_ruby_dir) { Pathname.new('/home/git/gdk/gitaly/ruby') }
+
+  describe '#diagnose' do
+    it 'is a no-op' do
+      expect(subject.diagnose).to be_nil
+    end
+  end
+
+  describe '#success?' do
+    context 'when gitaly has a BUNDLE_PATH configured' do
+      it 'returns false' do
+        expect_bundle_path_not_set(gitlab_dir)
+        expect_bundle_path_set(gitaly_ruby_dir)
+
+        expect(subject).not_to be_success
+      end
+    end
+
+    context "when gitlab and gitaly don't have BUNDLE_PATH configured" do
+      it 'returns true' do
+        expect_bundle_path_not_set(gitlab_dir)
+        expect_bundle_path_not_set(gitaly_ruby_dir)
+
+        expect(subject).to be_success
+      end
+    end
+  end
+
+  describe '#detail' do
+    context 'when gitaly has a BUNDLE_PATH configured' do
+      it 'returns a message' do
+        expect_bundle_path_not_set(gitlab_dir)
+        expect_bundle_path_set(gitaly_ruby_dir)
+
+        expect(subject.detail).to match(/#{gitaly_ruby_dir} appears to have BUNDLE_PATH configured/)
+      end
+    end
+
+    context "when gitlab and gitaly don't have BUNDLE_PATH configured" do
+      it 'returns no message' do
+        expect_bundle_path_not_set(gitlab_dir)
+        expect_bundle_path_not_set(gitaly_ruby_dir)
+
+        expect(subject.detail).to be_nil
+      end
+    end
+  end
+
+  def expect_bundle_path_not_set(chdir)
+    expect_shellout(chdir, stdout: 'You have not configured a value for `PATH`')
+  end
+
+  def expect_bundle_path_set(chdir)
+    expect_shellout(chdir, stdout: 'Set for your local app (<path>/.bundle/config): "vendor/bundle"')
+  end
+
+  def expect_shellout(chdir, success: true, stdout: '', stderr: '')
+    shellout = double('Shellout', try_run: nil, read_stdout: stdout, read_stderr: stderr, success?: success)
+    expect(Shellout).to receive(:new).with('bundle config get PATH', chdir: chdir).and_return(shellout)
+    expect(shellout).to receive(:execute).with(display_output: false).and_return(shellout)
+  end
+end
