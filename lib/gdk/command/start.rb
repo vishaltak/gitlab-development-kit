38183447e44ee7932650c9c6f3e25d3d423e4d11
Evan Read <eread@gitlab.com>
2022-02-22 15:12:25 +1000

    Add notice for container registry when starting GDK

--- lib/gdk/command/start.rb
+++ lib/gdk/command/start.rb
@@ -51,6 +51,7 @@ module GDK
         GDK::Output.notice("GitLab Agent Server (KAS) will be available at #{config.gitlab_k8s_agent.__url_for_agentk}.") if config.gitlab_k8s_agent?
         GDK::Output.notice("Prometheus will be available at http://#{config.hostname}:#{config.prometheus.port}.") if config.prometheus?
         GDK::Output.notice("Grafana will be available at http://#{config.hostname}:#{config.grafana.port}.") if config.grafana?
+        GDK::Output.notice("A container registry will be available at #{config.registry.host}:#{config.registry.port}.") if config.registry?
       end
     end
   end
