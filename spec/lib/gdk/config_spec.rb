85993a573d28e6e5be8260d3fab6e09363367af4
Thong Kuah <tkuah@gitlab.com>
2022-03-03 09:46:24 +1300

    Remove un-used config separate_db_schemas
    This config was an experiment that did not progress

--- spec/lib/gdk/config_spec.rb
+++ spec/lib/gdk/config_spec.rb
@@ -1176,12 +1176,6 @@ RSpec.describe GDK::Config do
         end
       end
 
-      describe 'separate_db_schemas' do
-        it 'is disabled by default' do
-          expect(config.gitlab.rails.separate_db_schemas).to be(false)
-        end
-      end
-
       describe 'multiple_databases' do
         it 'is disabled by default' do
           expect(config.gitlab.rails.multiple_databases).to be(false)
