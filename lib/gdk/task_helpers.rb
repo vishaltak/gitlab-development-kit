3562f6aee9024a93e7020b26de83c893583c506a
Gabriel Mazetto <gabriel@gitlab.com>
2022-04-28 18:23:27 +0200

    Refactor RailsMigration into TaskHelpers
    This creates a designated area for Helper classes used in
    tasks.
    
    Having it separate from the main flow is important as those
    helpers have different flows and expectations.
    
    A task helper can terminate the execution with an `exit()`
    code with makes it not suitable for use as part of the
    normal GDK API.

---
+++ lib/gdk/task_helpers.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'gdk'
+
+module GDK
+  # TaskHelpers are used by raketasks to include functionality that would not
+  # make sense to be part of "regular GDK" API.
+  #
+  # There is also expectation that because this is tightly coupled with tasks
+  # code executed here is allowed to terminate the flow with `exit()` or
+  # raise unhandled exceptions.
+  #
+  # IMPORTANT: Other parts of the codebase should NEVER rely on code inside
+  # TaskHelpers.
+  module TaskHelpers
+    autoload :RailsMigration, 'gdk/task_helpers/rails_migration'
+  end
+end
