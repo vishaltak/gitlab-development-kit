c77e1278fb1324c5b1edaebe32c5582f6da0878f
Patrick Steinhardt <psteinhardt@gitlab.com>
2022-05-04 10:07:40 +0200

    gitaly: Shorten default runtime dir for Gitaly nodes
    When setting up Gitaly nodes either as standalone node on in a cluster,
    then we create separate runtime directories for each of them by default.
    This isn't needed though: Gitaly will create directories keyed by the
    server's PID, so we create separate directories for each of the Gitaly
    nodes anyway. So the current config will end up with a directory path of
    either:
    
      - `tmp/gitaly/gitaly-${N}/gitaly-${PID}` in the case of a standalone
        Gitaly node.
    
      - `tmp/praefect/gitaly-${N}/gitaly-${PID}` in the case of a Gitaly
        Cluster node.
    
    Both paths will easily hit the socket path length limits of  Unix
    systems.
    
    Let's configure all Gitaly nodes to share the same runtime directory
    `tmp/` instead to avoid hitting this limit.

--- lib/gdk/config.rb
+++ lib/gdk/config.rb
@@ -523,7 +523,7 @@ module GDK
       path(:log_dir) { config.gdk_root.join('log', 'gitaly') }
       path(:storage_dir) { config.repositories_root }
       path(:repository_storages) { config.repository_storages }
-      path(:runtime_dir) { config.gdk_root.join('tmp', 'gitaly') }
+      path(:runtime_dir) { config.gdk_root.join('tmp') }
       string(:auth_token) { '' }
       bool(:auto_update) { true }
       integer(:storage_count) { 1 }
@@ -563,7 +563,7 @@ module GDK
         string(:storage) { "praefect-internal-#{i}" }
         path(:storage_dir) { config.repositories_root }
         path(:repository_storages) { config.repository_storages }
-        path(:runtime_dir) { config.gdk_root.join('tmp', 'praefect', "gitaly-#{i}") }
+        path(:runtime_dir) { config.gdk_root.join('tmp') }
         settings_array :__storages, size: 1 do |j|
           string(:name) { parent.parent.storage }
           path(:path) { i.zero? && j.zero? ? parent.parent.storage_dir : File.join(parent.parent.repository_storages, parent.parent.service_name, name) }
