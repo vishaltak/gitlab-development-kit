98bada2433e9ef9a1bf862b5ae5c329f414d5dfe
Ash McKenzie <amckenzie@gitlab.com>
2022-05-06 12:15:34 +1000

    Extend 'gdk open' command to test URL first

--- lib/gdk/command/open.rb
+++ lib/gdk/command/open.rb
@@ -5,10 +5,32 @@ module GDK
     # Handles `gdk reconfigure` command execution
     class Open < BaseCommand
       def run(_ = [])
-        exec("#{open_command} '#{config.__uri}'")
+        if test_url.check_url_oneshot
+          open_exec
+          return true
+        end
+
+        unless test_url.wait(verbose: false)
+          GDK::Output.error('GDK is not up. Please run `gdk start` and try again.')
+          return false
+        end
+
+        open_exec
+        true
+      rescue Interrupt
+        # CTRL-C was pressed
+        true
       end
 
       private
+
+      def test_url
+        @test_url ||= GDK::TestURL.new
+      end
+
+      def open_exec
+        exec("#{open_command} '#{config.__uri}'")
+      end
 
       def open_command
         @open_command ||= config.__platform_linux? ? 'xdg-open' : 'open'
--- spec/lib/gdk/command/open_spec.rb
+++ spec/lib/gdk/command/open_spec.rb
@@ -4,28 +4,62 @@ require 'spec_helper'
 
 RSpec.describe GDK::Command::Open do
   let(:host_os) { nil }
+  let(:wait_result) { nil }
+  let(:check_url_oneshot_result) { nil }
+
+  let(:test_url_double) { instance_double(GDK::TestURL) }
 
   before do
     allow(RbConfig::CONFIG).to receive(:[]).with('host_os').and_return(host_os)
+
+    allow(GDK::TestURL).to receive(:new).and_return(test_url_double)
+    allow(test_url_double).to receive(:check_url_oneshot).and_return(check_url_oneshot_result)
+    allow(test_url_double).to receive(:wait).and_return(wait_result)
   end
 
-  context 'when Linux' do
-    let(:host_os) { 'Linux' }
+  context 'when GDK is not up' do
+    let(:check_url_oneshot_result) { false }
+    let(:wait_result) { false }
 
-    it 'calls open <GDK_URL>' do
-      expect(subject).to receive(:exec).with("xdg-open 'http://127.0.0.1:3000'")
+    it 'advises GDK is not up and returns' do
+      result = nil
+      expect { result = subject.run }.to output(/GDK is not up. Please run `gdk start` and try again./).to_stderr
+      expect(result).to be_falsey
+    end
+  end
+
+  context 'when GDK is not up initially, but then comes up' do
+    let(:check_url_oneshot_result) { false }
+    let(:wait_result) { true }
+
+    it 'advises GDK is not up and returns' do
+      expect(subject).to receive(:exec).with("open 'http://127.0.0.1:3000'")
 
       subject.run
     end
   end
 
-  context 'when not Linux' do
-    let(:host_os) { 'Darwin' }
+  context 'when GDK is up' do
+    let(:check_url_oneshot_result) { true }
 
-    it 'calls open <GDK_URL>' do
-      expect(subject).to receive(:exec).with("open 'http://127.0.0.1:3000'")
+    context 'when Linux' do
+      let(:host_os) { 'Linux' }
 
-      subject.run
+      it 'calls open <GDK_URL>' do
+        expect(subject).to receive(:exec).with("xdg-open 'http://127.0.0.1:3000'")
+
+        subject.run
+      end
+    end
+
+    context 'when not Linux' do
+      let(:host_os) { 'Darwin' }
+
+      it 'calls open <GDK_URL>' do
+        expect(subject).to receive(:exec).with("open 'http://127.0.0.1:3000'")
+
+        subject.run
+      end
     end
   end
 end
