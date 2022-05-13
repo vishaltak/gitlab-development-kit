c5a7a80b81343ed6f02369936ced2aa8541a5902
Ash McKenzie <amckenzie@gitlab.com>
2022-05-13 16:54:33 +1000

    Move Start#print_url_ready_message into BaseCommand

--- lib/gdk/command/base_command.rb
+++ lib/gdk/command/base_command.rb
@@ -36,6 +36,16 @@ module GDK
           - Run 'gdk pristine' which will restore your GDK to a pristine state.
         HELP_MESSAGE
       end
+
+      def print_url_ready_message
+        GDK::Output.puts
+        GDK::Output.notice("GitLab will be available at #{config.__uri}.")
+        GDK::Output.notice("GitLab Docs will be available at http://#{config.hostname}:#{config.gitlab_docs.port}.") if config.gitlab_docs.enabled?
+        GDK::Output.notice("GitLab Agent Server (KAS) will be available at #{config.gitlab_k8s_agent.__url_for_agentk}.") if config.gitlab_k8s_agent?
+        GDK::Output.notice("Prometheus will be available at http://#{config.hostname}:#{config.prometheus.port}.") if config.prometheus?
+        GDK::Output.notice("Grafana will be available at http://#{config.hostname}:#{config.grafana.port}.") if config.grafana?
+        GDK::Output.notice("A container registry will be available at #{config.registry.host}:#{config.registry.port}.") if config.registry?
+      end
     end
   end
 end
