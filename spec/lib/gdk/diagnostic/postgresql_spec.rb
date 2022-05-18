3711a21708ca87815f56807c8efe17ff955833e1
Ash McKenzie <amckenzie@gitlab.com>
2022-05-18 17:16:45 +1000

    Move socket check logic into PostgreSQL diagnostic

--- spec/lib/gdk/diagnostic/postgresql_spec.rb
+++ spec/lib/gdk/diagnostic/postgresql_spec.rb
@@ -25,45 +25,76 @@ RSpec.describe GDK::Diagnostic::PostgreSQL do # rubocop:disable RSpec/FilePath
       stub_psql_version('psql (PostgreSQL) 11.9', success: psql_success)
     end
 
-    context 'when psql --version matches PG_VERSION' do
-      it 'returns true' do
-        expect(subject).to be_success
-      end
-    end
-
-    context 'when psql --version differs' do
+    context 'versions testing' do
       before do
-        stub_psql_version('psql (PostgreSQL) 12.8', success: psql_success)
+        allow(subject).to receive(:can_create_postgres_socket?).and_return(true)
       end
 
-      it 'returns false' do
-        expect(subject).not_to be_success
-      end
-    end
-
-    context 'when psql does not succeed' do
-      let(:psql_success) { false }
-
-      it 'returns false' do
-        expect(subject).not_to be_success
-      end
-    end
-
-    context 'when psql --version is 9.6' do
-      before do
-        stub_psql_version('psql (PostgreSQL) 9.6.18', success: psql_success)
+      context 'when psql --version matches PG_VERSION' do
+        it 'returns true' do
+          expect(subject).to be_success
+        end
       end
 
-      it 'returns false' do
-        expect(subject).not_to be_success
-      end
-
-      context 'when data version is 9.6' do
+      context 'when psql --version differs' do
         before do
-          allow(subject).to receive(:data_dir_version).and_return(9.6)
+          stub_psql_version('psql (PostgreSQL) 12.8', success: psql_success)
         end
 
         it 'returns false' do
+          expect(subject).not_to be_success
+        end
+      end
+
+      context 'when psql does not succeed' do
+        let(:psql_success) { false }
+
+        it 'returns false' do
+          expect(subject).not_to be_success
+        end
+      end
+
+      context 'when psql --version is 9.6' do
+        before do
+          stub_psql_version('psql (PostgreSQL) 9.6.18', success: psql_success)
+        end
+
+        it 'returns false' do
+          expect(subject).not_to be_success
+        end
+
+        context 'when data version is 9.6' do
+          before do
+            allow(subject).to receive(:data_dir_version).and_return(9.6)
+          end
+
+          it 'returns false' do
+            expect(subject).to be_success
+          end
+        end
+      end
+    end
+
+    context 'socket creation testing' do
+      let(:can_create_socket) { nil }
+
+      before do
+        allow(subject).to receive(:versions_ok?).and_return(true)
+        stub_can_create_socket(can_create_socket)
+      end
+
+      context 'when the GDK base directory length is too long' do
+        let(:can_create_socket) { false }
+
+        it 'returns false' do
+          expect(subject).not_to be_success
+        end
+      end
+
+      context 'when the GDK base directory length is OK' do
+        let(:can_create_socket) { true }
+
+        it 'returns true' do
           expect(subject).to be_success
         end
       end
@@ -81,23 +112,59 @@ RSpec.describe GDK::Diagnostic::PostgreSQL do # rubocop:disable RSpec/FilePath
       end
     end
 
-    context 'when unsuccessful' do
+    context 'versions testing' do
       before do
-        allow(subject).to receive(:success?).and_return(false)
-        allow(subject).to receive(:psql_version).and_return(11.8)
-        allow(subject).to receive(:data_dir_version).and_return(12)
+        allow(subject).to receive(:can_create_postgres_socket?).and_return(true)
       end
 
-      it 'returns help message' do
-        expected = <<~MESSAGE
-        `psql` is version 11.8, but your PostgreSQL data dir is using version 12.
-        Check that your PATH is pointing to the right PostgreSQL version, or see the PostgreSQL upgrade guide:
-        https://gitlab.com/gitlab-org/gitlab-development-kit/-/blob/main/doc/howto/postgresql.md#upgrade-postgresql
-        MESSAGE
+      context 'when unsuccessful' do
+        before do
+          allow(subject).to receive(:success?).and_return(false)
+          allow(subject).to receive(:psql_version).and_return(11.8)
+          allow(subject).to receive(:data_dir_version).and_return(12)
+        end
 
-        expect(subject.detail).to eq(expected)
+        it 'returns help message' do
+          expected = <<~MESSAGE
+          `psql` is version 11.8, but your PostgreSQL data dir is using version 12.
+
+          Check that your PATH is pointing to the right PostgreSQL version, or see the PostgreSQL upgrade guide:
+          https://gitlab.com/gitlab-org/gitlab-development-kit/-/blob/main/doc/howto/postgresql.md#upgrade-postgresql
+          MESSAGE
+
+          expect(subject.detail).to eq(expected)
+        end
       end
     end
+
+    context 'socket creation testing' do
+      before do
+        allow(subject).to receive(:versions_ok?).and_return(true)
+      end
+
+      context 'when unsuccessful' do
+        before do
+          stub_can_create_socket(false)
+        end
+
+        it 'returns help message' do
+          expected = <<~MESSAGE
+          GDK directory's character length (13) is too long to support the creation
+          of a UNIX socket for Postgres:
+
+            /home/git/gdk
+
+          Try using a shorter directory path for GDK or use TCP for Postgres.
+          MESSAGE
+
+          expect(subject.detail).to eq(expected)
+        end
+      end
+    end
+  end
+
+  def stub_can_create_socket(value)
+    allow(subject).to receive(:can_create_socket?).with('/home/git/gdk/postgresql_.s.PGSQL.XXXXX').and_return(value)
   end
 
   def stub_data_version(version)
