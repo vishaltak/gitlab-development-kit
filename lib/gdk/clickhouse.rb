54a866303db123f04d806fb22c29db6c219556a9
Gabriel Mazetto <gabriel@gitlab.com>
2022-05-25 06:20:54 +0200

    Added `installed?` and `current_version` methods for ClickHouse class

--- lib/gdk/clickhouse.rb
+++ lib/gdk/clickhouse.rb
@@ -3,11 +3,26 @@
 module GDK
   # Provides ClickHouse utility methods
   class Clickhouse
+    VERSION_REGEXP = /ClickHouse (?:client|server) version (?<version>[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/.freeze
+
     def client_cmd(args = [])
       cmd = [config.bin.to_s]
       cmd << 'client'
       cmd << "--port=#{config.tcp_port}"
       (cmd + args).flatten
+    end
+
+    def installed?
+      File.exist?(config.bin)
+    end
+
+    def current_version
+      return unless installed?
+
+      output = Shellout.new(config.bin.to_s, 'server', '--version').try_run.strip.chomp
+      matched = output.match(VERSION_REGEXP)
+
+      matched[:version]
     end
 
     private
--- spec/lib/gdk/clickhouse_spec.rb
+++ spec/lib/gdk/clickhouse_spec.rb
@@ -25,4 +25,44 @@ RSpec.describe GDK::Clickhouse do
       expect(subject.client_cmd).to include('--port=9898')
     end
   end
+
+  describe '#installed?' do
+    it 'returns true when there is a file at location specified in bin config' do
+      mock_installed
+
+      expect(subject.installed?).to be_truthy
+    end
+
+    it 'returns false when binary cant be found' do
+      mock_not_installed
+
+      expect(subject.installed?).to be_falsey
+    end
+  end
+
+  describe '#current_version' do
+    it 'returns version when it is installed' do
+      shellout_double = instance_double(Shellout)
+      allow(shellout_double).to receive(:try_run).and_return('ClickHouse server version 22.1.2.3')
+
+      mock_installed
+      allow(Shellout).to receive(:new).with(GDK.config.clickhouse.bin.to_s, 'server', '--version').and_return(shellout_double)
+
+      expect(subject.current_version).to eq('22.1.2.3')
+    end
+
+    it 'doesnt return anything if not installed' do
+      mock_not_installed
+
+      expect(subject.current_version).to be_nil
+    end
+  end
+
+  def mock_installed
+    allow(File).to receive(:exist?).with(GDK.config.clickhouse.bin).and_return(true)
+  end
+
+  def mock_not_installed
+    allow(File).to receive(:exist?).with(GDK.config.clickhouse.bin).and_return(false)
+  end
 end
