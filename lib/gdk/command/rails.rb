9876a9fce6428994a24c290c7106d9a92a75054a
Marco Zille <marco.zille@gmail.com>
2022-05-27 10:54:21 +0200

    Added rails command

---
+++ lib/gdk/command/rails.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module GDK
+  module Command
+    # Handles `gdk rails <command> [<args>]` command execution
+    class Rails < BaseCommand
+      def run(args = [])
+        GDK::Output.abort('Usage: gdk rails <command> [<args>]') if args.length == 0
+
+        execute_command!(args)
+      end
+
+      private
+
+      def execute_command!(args)
+        exec(
+          { 'RAILS_ENV' => 'development' },
+          *generate_command(args),
+          chdir: GDK.root.join('gitlab')
+        )
+      end
+
+      def generate_command(args)
+        %w[bundle exec rails] + args
+      end
+    end
+  end
+end
