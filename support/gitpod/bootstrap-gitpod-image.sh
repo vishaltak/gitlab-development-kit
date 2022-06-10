ca9577d8c02083bf23e36fe0f304a18b1989112b
Ash McKenzie <amckenzie@gitlab.com>
2022-06-10 16:21:18 +1000

    Allow Gitpod image generation to use MR branch

--- support/gitpod/bootstrap-gitpod-image.sh
+++ support/gitpod/bootstrap-gitpod-image.sh
@@ -18,6 +18,7 @@ sudo chown -R gitpod:gitpod /workspace
 cd /workspace
 git clone https://gitlab.com/gitlab-org/gitlab-development-kit.git
 cd gitlab-development-kit
+[[ -n "${CI_COMMIT_SHA:-}" ]] && git checkout "${CI_COMMIT_SHA}"
 make bootstrap
 
 # Set asdf dir correctly
