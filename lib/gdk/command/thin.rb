a63aa5476096e022460913f7ce379adde2eb3651
Toon Claes <toon@gitlab.com>
2022-07-06 09:42:30 +0000

    Tweak help message when using gdk thin
    The gdk thin command no longer works. To make it easier for the user to
    copy/paste the new command, show it on a separate line.

--- lib/gdk/command/thin.rb
+++ lib/gdk/command/thin.rb
@@ -5,7 +5,9 @@ module GDK
     # Handles `gdk thin` command execution
     class Thin < BaseCommand
       def run(args = [])
-        GDK::Output.puts "gdk thin is deprecated. Use 'gdk rails s -e GITLAB_RAILS_RACK_TIMEOUT_ENABLE=false' instead."
+        GDK::Output.puts 'This command is deprecated. Use the following command instead:'
+        GDK::Output.puts
+        GDK::Output.puts '    gdk rails s -e GITLAB_RAILS_RACK_TIMEOUT_ENABLE=false'
 
         false
       end
--- spec/lib/gdk/command/thin_spec.rb
+++ spec/lib/gdk/command/thin_spec.rb
@@ -6,9 +6,10 @@ RSpec.describe GDK::Command::Thin do
   let(:args) { [] }
 
   it 'displays deprecation warning and returns false' do
-    stub_const('MSG', "gdk thin is deprecated. Use 'gdk rails s -e GITLAB_RAILS_RACK_TIMEOUT_ENABLE=false' instead.")
+    msg = 'This command is deprecated. Use the following command instead:'
 
-    expect(GDK::Output).to receive(:puts).with(MSG)
+    allow(GDK::Output).to receive(:puts)
+    expect(GDK::Output).to receive(:puts).with(msg)
     expect(subject.run(args)).to be(false)
   end
 end
