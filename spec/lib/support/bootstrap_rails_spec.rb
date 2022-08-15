fad7446228cd67a406c6ee7494d6290a585719ff
Ash McKenzie <amckenzie@gitlab.com>
2022-08-15 15:40:01 +1000

    Support setting feature flags before DB setup

--- spec/lib/support/bootstrap_rails_spec.rb
+++ spec/lib/support/bootstrap_rails_spec.rb
@@ -57,12 +57,13 @@ RSpec.describe Support::BootstrapRails do
           allow(postgres_mock).to receive(:db_exists?).with('gitlabhq_development_ci').and_return(gitlabhq_development_ci_db_exists)
         end
 
-        context 'when gitlabhq_development gitlabhq_development_ci DBs already exist' do
+        context 'when gitlabhq_development and gitlabhq_development_ci DBs already exist' do
           let(:gitlabhq_development_db_exists) { true }
           let(:gitlabhq_development_ci_db_exists) { true }
 
           it 'advises and skips further logic' do
             expect(GDK::Output).to receive(:info).with('gitlabhq_development exists, nothing to do here.')
+
             expect(GDK::Output).to receive(:info).with('gitlabhq_development_ci exists, nothing to do here.')
 
             subject
@@ -74,32 +75,36 @@ RSpec.describe Support::BootstrapRails do
           let(:gitlabhq_development_ci_db_exists) { false }
 
           context 'attempts to setup the gitlabhq_development DB' do
-            context 'but `rake `db:setup fails' do
+            context 'but `rake `db:reset fails' do
               it 'exits with a status code of 1' do
-                stub_rake_task(described_class::RAKE_DEV_SETUP_CMD, success: false)
+                stub_shellout(described_class::RAKE_DEV_DB_RESET_CMD, success: false)
 
                 expect { subject }
-                  .to output(/The command "bundle exec rake dev:setup" failed/).to_stderr
+                  .to output(/The command '#{described_class::RAKE_DEV_DB_RESET_CMD.join(' ')}' failed/).to_stderr
                   .and raise_error(SystemExit) { |error| expect(error.status).to eq(1) }
               end
             end
 
-            context 'when `rake db:setup` succeeds' do
-              context 'but `rake ev:copy_db:ci` fails' do
+            context 'when `rake db:reset` succeeds' do
+              context 'but `rake dev:copy_db:ci` fails' do
                 it 'exits with a status code of 0' do
-                  stub_rake_task(described_class::RAKE_DEV_SETUP_CMD, success: true)
-                  stub_rake_task(described_class::RAKE_COPY_DB_CI_CMD, success: false)
+                  stub_shellout(described_class::RAKE_DEV_DB_RESET_CMD, success: true)
+                  stub_shellout(described_class::RAILS_RUNNER_SKIP_RUGGED_AUTO_DETECT_CMD, success: true)
+                  stub_shellout(described_class::RAKE_DEV_DB_SEED_CMD, success: true)
+                  stub_shellout(described_class::RAKE_COPY_DB_CI_CMD, success: false)
 
                   expect { subject }
-                    .to output(/The command "bundle exec rake dev:copy_db:ci" failed/).to_stderr
+                    .to output(/The command '#{described_class::RAKE_COPY_DB_CI_CMD.join(' ')}' failed/).to_stderr
                     .and raise_error(SystemExit) { |error| expect(error.status).to eq(1) }
                 end
               end
 
-              context 'and `rake ev:copy_db:ci` succeeds' do
+              context 'and `rake dev:copy_db:ci` succeeds' do
                 it 'exits with a status code of 0' do
-                  stub_rake_task(described_class::RAKE_DEV_SETUP_CMD, success: true)
-                  stub_rake_task(described_class::RAKE_COPY_DB_CI_CMD, success: true)
+                  stub_shellout(described_class::RAKE_DEV_DB_RESET_CMD, success: true)
+                  stub_shellout(described_class::RAILS_RUNNER_SKIP_RUGGED_AUTO_DETECT_CMD, success: true)
+                  stub_shellout(described_class::RAKE_DEV_DB_SEED_CMD, success: true)
+                  stub_shellout(described_class::RAKE_COPY_DB_CI_CMD, success: true)
 
                   expect { subject }.not_to raise_error
                 end
@@ -111,9 +116,9 @@ RSpec.describe Support::BootstrapRails do
     end
   end
 
-  def stub_rake_task(rake_task, success:)
+  def stub_shellout(cmd, success:)
     shellout_double = instance_double(Shellout, success?: success)
-    allow(Shellout).to receive(:new).with(rake_task).and_return(shellout_double)
+    allow(Shellout).to receive(:new).with(cmd).and_return(shellout_double)
     allow(shellout_double).to receive(:execute).with(retry_attempts: 3).and_return(shellout_double)
   end
 end
