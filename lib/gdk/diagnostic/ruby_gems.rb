f178db63c73c3bb5f7f23bda4c492519ca218709
Ash McKenzie <amckenzie@gitlab.com>
2023-02-08 22:46:42 +0000

    Resolve "icu4c updates break brand new GDK installations"

--- lib/gdk/diagnostic/ruby_gems.rb
+++ lib/gdk/diagnostic/ruby_gems.rb
@@ -8,29 +8,55 @@ module GDK
       TITLE = 'Ruby Gems'
       GITLAB_GEMS_TO_CHECK = %w[charlock_holmes ffi gpgme pg oj].freeze
 
+      def initialize(allow_gem_not_installed: false)
+        @allow_gem_not_installed = allow_gem_not_installed
+
+        super()
+      end
+
       def success?
         failed_to_load_gitlab_gems.empty?
       end
 
       def detail
-        return gitlab_error_message unless failed_to_load_gitlab_gems.empty?
+        return gitlab_error_message unless success?
       end
 
       private
 
-      def failed_to_load_gitlab_gems
-        @failed_to_load_gitlab_gems ||= GITLAB_GEMS_TO_CHECK.reject do |name|
-          gem_ok?(name, config.gitlab.dir.to_s)
-        end
+      attr_reader :allow_gem_not_installed
+
+      def allow_gem_not_installed?
+        @allow_gem_not_installed == true
       end
 
-      def gem_ok?(name, chdir)
-        cmd = "#{config.gdk_root.join('support', 'bundle-exec')} gem list -i #{name}"
+      def failed_to_load_gitlab_gems
+        @failed_to_load_gitlab_gems ||= GITLAB_GEMS_TO_CHECK.reject { |name| gem_ok?(name) }
+      end
+
+      def gem_ok?(name)
+        # We need to support the situation where it's OK if a Ruby gem is not
+        # installed because we could be about to install the GDK for the very
+        # first time and the Ruby gem won't be installed.
+        gem_installed?(name) ? gem_loads_ok?(name) : allow_gem_not_installed?
+      end
+
+      def bundle_exec_cmd
+        @bundle_exec_cmd ||= config.gdk_root.join('support', 'bundle-exec')
+      end
+
+      def gem_installed?(name)
+        exec_cmd("#{bundle_exec_cmd} gem list -i #{name}")
+      end
+
+      def gem_loads_ok?(name)
+        exec_cmd("#{bundle_exec_cmd} ruby -r #{name} -e 'nil'")
+      end
+
+      def exec_cmd(cmd)
         GDK::Output.debug("cmd=[#{cmd}]")
 
-        sh = Shellout.new(cmd, chdir: chdir)
-        sh.try_run
-        sh.success?
+        Shellout.new(cmd, chdir: config.gitlab.dir.to_s).execute(display_output: false, display_error: false).success?
       end
 
       def gitlab_error_message
