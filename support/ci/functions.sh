edb387ccdc939c9b3a4ac216bb48dfe258826bf2
Manoj M J <mmj@gitlab.com>
2023-07-21 15:29:09 +0200

    Make changes

--- support/ci/functions.sh
+++ support/ci/functions.sh
@@ -104,13 +104,6 @@ reconfigure() {
   gdk reconfigure
 }
 
-update_webpack_config() {
-  cd_into_checkout_path
-
-  echo "> Setting webpack config.."
-  gdk config set webpack.live_reload false && gdk config set webpack.sourcemaps false
-}
-
 reset_data() {
   cd_into_checkout_path
 
