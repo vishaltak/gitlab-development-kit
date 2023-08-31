45f74ca334658e43007d9014f402c9b5bccc8843
Sylvester Chin <schin@gitlab.com>
2023-08-31 12:16:04 +0800

    Allow ConfigType::Hash to parse strings

---
+++ spec/lib/gdk/config_type/hash_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe GDK::ConfigType::Hash do
+  let(:value) { nil }
+  let(:key) { 'test_key' }
+  let(:yaml) { { key => value } }
+  let(:builder) { GDK::ConfigType::Builder.new(key: key, klass: described_class, **{}, &proc { value }) }
+
+  subject { described_class.new(parent: GDK.config, builder: builder) }
+
+  before do
+    stub_pg_bindir
+    stub_gdk_yaml(yaml)
+  end
+
+  describe '#parse' do
+    context 'when value is initialized with an string' do
+      context 'when string is json parseable' do
+        let(:value) { '{"test": "hash"}' }
+
+        it 'returns parsed value' do
+          expect(subject.parse(value)).to eq({ 'test' => 'hash' })
+        end
+      end
+
+      context 'when string is not json parseable' do
+        let(:value) { '.' }
+
+        it 'raises parser error' do
+          expect { subject.parse(value) }.to raise_error(GDK::StandardErrorWithMessage)
+        end
+      end
+    end
+
+    context 'when value is initialized with an array' do
+      let(:value) { [] }
+
+      it 'returns parsed value' do
+        expect(subject.parse(value)).to eq({})
+      end
+    end
+  end
+end
