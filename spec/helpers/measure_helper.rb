7b5b16bf09245f6428b6d47340e56d77f85416b1
Nao Hashizume <nhashizume@gitlab.com>
2023-09-27 14:57:21 +0000

    Refactor measure_url_spec and measure_workflow_spec

--- spec/helpers/measure_helper.rb
+++ spec/helpers/measure_helper.rb
@@ -25,6 +25,57 @@ module MeasureHelper
     end
   end
 
+  shared_context 'Docker and GDK are running' do
+    let(:docker_running) { true }
+
+    before do
+      stub_gdk_check(is_running: true)
+      stub_const('GDK::Command::MeasureBase::SITESPEED_DOCKER_TAG', '1.2.3')
+    end
+  end
+
+  shared_examples 'runs sitespeed via Docker' do |platform, args_type, args|
+    let(:branch_name) { 'some-branch-name' }
+    let(:is_linux) { platform == 'linux' }
+
+    before do
+      stub_git_rev_parse(branch_name: branch_name)
+    end
+
+    it "runs sitespeed via Docker on a #{platform} system", :hide_stdout do
+      freeze_time do
+        allow(GDK::Machine).to receive(:linux?).and_return(is_linux)
+        report_file_path = "sitespeed-result/#{branch_name}_#{Time.now.strftime('%F-%H-%M-%S')}"
+        network_host = is_linux ? '--network=host' : ''
+
+        docker_command = case args_type
+                         when 'urls'
+                           docker_command_for_urls(args, network_host, report_file_path)
+                         when 'workflows'
+                           docker_command_for_workflows(args, network_host, report_file_path)
+                         end
+
+        shellout_docker_run_double = instance_double(Shellout, stream: '', success?: true)
+        expect(Shellout).to receive(:new).with(docker_command).and_return(shellout_docker_run_double)
+
+        shellout_open_double = instance_double(Shellout, run: true, success?: true)
+        expect(Shellout).to receive(:new).with("open ./#{report_file_path}/index.html").and_return(shellout_open_double)
+
+        subject.run(args)
+      end
+    end
+  end
+
+  def docker_command_for_urls(urls, network_host, report_file_path)
+    url_paths = urls.map { |url| "http://host.docker.internal:3000#{url}" }.join(' ')
+    %(docker run #{network_host} --cap-add=NET_ADMIN --shm-size 2g --rm -v "$(pwd):/sitespeed.io" sitespeedio/sitespeed.io:1.2.3 -b chrome -n 4 -c cable --cookie perf_bar_enabled=false --cpu --outputFolder #{report_file_path} #{url_paths})
+  end
+
+  def docker_command_for_workflows(workflows, network_host, report_file_path)
+    workflow_paths = workflows.map { |workflow| "support/measure_scripts/#{workflow}.js" }.join(' ')
+    %(docker run #{network_host} --cap-add=NET_ADMIN --shm-size 2g --rm -v "$(pwd):/sitespeed.io" sitespeedio/sitespeed.io:1.2.3 -b chrome -n 4 -c cable --cookie perf_bar_enabled=false --cpu --outputFolder #{report_file_path} --multi --spa #{workflow_paths})
+  end
+
   def stub_git_rev_parse(branch_name:)
     shellout_double = instance_double(Shellout, run: branch_name)
     allow(Shellout).to receive(:new).with('git rev-parse --abbrev-ref HEAD', chdir: GDK.config.gitlab.dir).and_return(shellout_double)
