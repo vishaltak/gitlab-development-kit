9dfb78e35a513dde6f8dbd9d7d4bd0d1d1bc626c
Gabriel Mazetto <gabriel@gitlab.com>
2023-10-12 15:55:02 +0200

    Exclude geo boostrap on primary node

--- support/makefiles/Makefile.postgresql-geo.mk
+++ support/makefiles/Makefile.postgresql-geo.mk
@@ -1,14 +1,18 @@
 postgresql/geo: postgresql-geo/data postgresql/geo/port postgresql/geo/seed-data
 
 postgresql-geo/data:
-ifeq ($(geo_enabled),true)
+ifneq ($(geo_enabled),true)
+	@true
+else ifeq ($(geo_secondary), true)
 	$(Q)${postgresql_bin_dir}/initdb --locale=C -E utf-8 postgresql-geo/data
 else
-	@true
+  @true
 endif
 
 postgresql/geo/port: postgresql-geo/data
-ifeq ($(geo_enabled),true)
+ifneq ($(geo_enabled),true)
+	@true
+else ifeq ($(geo_secondary), true)
 	$(Q)support/postgres-port ${postgresql_geo_dir} ${postgresql_geo_port}
 else
 	@true
@@ -18,7 +22,13 @@ postgresql/geo/Procfile:
 	$(Q)grep '^postgresql-geo:' Procfile || (printf ',s/^#postgresql-geo/postgresql-geo/\nwq\n' | ed -s Procfile)
 
 postgresql/geo/seed-data:
+ifneq ($(geo_enabled),true)
+	@true
+else ifeq ($(geo_secondary), true)
 	$(Q)support/bootstrap-geo
+else
+	@true
+endif
 
 postgresql-geo-replication-primary: postgresql-replication/config
 
