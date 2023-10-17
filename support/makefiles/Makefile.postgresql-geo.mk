0a5212524bfdd07f4f68a71867e4167f08bea0a4
Gabriel Mazetto <gabriel@gitlab.com>
2023-10-17 02:03:24 +0200

    Fix support files to use `$geo_secondary` as condition

--- support/makefiles/Makefile.postgresql-geo.mk
+++ support/makefiles/Makefile.postgresql-geo.mk
@@ -1,33 +1,33 @@
 postgresql/geo: postgresql-geo/data postgresql/geo/port postgresql/geo/seed-data
 
 postgresql-geo/data:
-ifneq ($(geo_enabled),true)
-	@true
-else ifeq ($(geo_secondary), true)
+ifeq ($(geo_enabled), true)
+ifeq ($(geo_secondary), true)
 	$(Q)${postgresql_bin_dir}/initdb --locale=C -E utf-8 postgresql-geo/data
 else
-  @true
+	@true
+endif
 endif
 
 postgresql/geo/port: postgresql-geo/data
-ifneq ($(geo_enabled),true)
-	@true
-else ifeq ($(geo_secondary), true)
+ifeq ($(geo_enabled), true)
+ifeq ($(geo_secondary), true)
 	$(Q)support/postgres-port ${postgresql_geo_dir} ${postgresql_geo_port}
 else
 	@true
+endif
 endif
 
 postgresql/geo/Procfile:
 	$(Q)grep '^postgresql-geo:' Procfile || (printf ',s/^#postgresql-geo/postgresql-geo/\nwq\n' | ed -s Procfile)
 
 postgresql/geo/seed-data:
-ifneq ($(geo_enabled),true)
-	@true
-else ifeq ($(geo_secondary), true)
+ifeq ($(geo_enabled), true)
+ifeq ($(geo_secondary), true)
 	$(Q)support/bootstrap-geo
 else
 	@true
+endif
 endif
 
 postgresql-geo-replication-primary: postgresql-replication/config
