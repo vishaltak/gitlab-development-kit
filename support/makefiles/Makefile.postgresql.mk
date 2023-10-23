7ac0f7a4c5ddba516988e8c51d3a41e1d33295af
Gabriel Mazetto <gabriel@gitlab.com>
2023-10-23 17:17:21 +0200

    Ensure praefect tasks runs only when praefect is enabled

--- support/makefiles/Makefile.postgresql.mk
+++ support/makefiles/Makefile.postgresql.mk
@@ -5,7 +5,7 @@ postgresql/data:
 	$(Q)${postgresql_bin_dir}/initdb --locale=C -E utf-8 ${postgresql_data_dir}
 
 .PHONY: _postgresql-seed-dbs
-_postgresql-seed-dbs: _postgresql-seed-dbs-heading _postgresql-seed-rails _postgresql-seed-praefect
+_postgresql-seed-dbs: _postgresql-seed-dbs-heading _postgresql-seed-praefect _postgresql-seed-rails
 
 .PHONY: _postgresql-seed-dbs-heading
 _postgresql-seed-dbs-heading:
@@ -14,14 +14,21 @@ _postgresql-seed-dbs-heading:
 	@echo "Ensuring necessary databases are setup and seeded"
 	@echo "${DIVIDER}"
 
+.PHONY: _posgresql-environment
+_postgresql-environment: Procfile postgresql/data postgresql-geo/data postgresql/geo/port
+	$(Q)gdk start db --quiet
+
 .PHONY: _postgresql-seed-rails
-_postgresql-seed-rails: _postgresql-seed-praefect
+_postgresql-seed-rails: _postgresql-environment
 	$(Q)support/bootstrap-rails
 
 .PHONY: _postgresql-seed-praefect
-_postgresql-seed-praefect: Procfile postgresql/data postgresql-geo/data postgresql/geo/port
-	$(Q)gdk start db --quiet
+_postgresql-seed-praefect: _postgresql-environment
+ifeq ($(praefect_enabled), true)
 	$(Q)support/bootstrap-praefect
+else
+	@true
+endif
 
 postgresql/port:
 	$(Q)support/postgres-port ${postgresql_dir} ${postgresql_port}
