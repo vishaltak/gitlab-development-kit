8e158c0118812a905abdc37fbf9334e6862e0729
Jaime Martinez <jmartinez@gitlab.com>
2023-11-13 09:53:57 -0600

    Update registry to v3.86.1-gitlab

--- spec/lib/gdk/config_spec.rb
+++ spec/lib/gdk/config_spec.rb
@@ -2058,7 +2058,7 @@ RSpec.describe GDK::Config do
     describe '#image' do
       context 'when no image is specified' do
         it 'returns the default image' do
-          expect(config.registry.image).to eq('registry.gitlab.com/gitlab-org/build/cng/gitlab-container-registry:v3.85.0-gitlab')
+          expect(config.registry.image).to eq('registry.gitlab.com/gitlab-org/build/cng/gitlab-container-registry:v3.86.1-gitlab')
         end
       end
     end
