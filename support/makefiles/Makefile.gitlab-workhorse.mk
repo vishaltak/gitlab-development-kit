1fb04666b341c7cb5f83e64a5536da6121892c73
Nao Hashizume <nhashizume@gitlab.com>
2024-01-03 17:19:11 +1000

    Add asdf-install target for gitlab/workhorse

--- support/makefiles/Makefile.gitlab-workhorse.mk
+++ support/makefiles/Makefile.gitlab-workhorse.mk
@@ -1,3 +1,5 @@
+workhorse_dir = ${gitlab_development_root}/gitlab/workhorse
+
 ifeq ($(workhorse_skip_setup),true)
 gitlab-workhorse-setup:
 	@echo
@@ -5,7 +7,7 @@ gitlab-workhorse-setup:
 	@echo "Skipping gitlab-workhorse setup due to option workhorse.skip_setup set to true"
 	@echo "${DIVIDER}"
 else
-gitlab-workhorse-setup: gitlab/workhorse/gitlab-workhorse gitlab/workhorse/config.toml
+gitlab-workhorse-setup: gitlab-workhorse-asdf-install gitlab/workhorse/gitlab-workhorse gitlab/workhorse/config.toml
 endif
 
 .PHONY: gitlab-workhorse-update
@@ -13,6 +15,18 @@ gitlab-workhorse-update: gitlab-workhorse-update-timed
 
 .PHONY: gitlab-workhorse-run
 gitlab-workhorse-update-run: gitlab-workhorse-clean-bin gitlab/workhorse/config.toml gitlab-workhorse-setup
+
+gitlab-workhorse-asdf-install:
+ifeq ($(asdf_opt_out),false)
+	@echo
+	@echo "${DIVIDER}"
+	@echo "Installing asdf tools from ${workhorse_dir}/.tool-versions"
+	@echo "${DIVIDER}"
+	$(Q)cd ${workhorse_dir} && ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="${workhorse_dir}/.tool-versions" asdf install
+	$(Q)cd ${workhorse_dir} && asdf reshim
+else
+	@true
+endif
 
 .PHONY: gitlab-workhorse-compile
 gitlab-workhorse-compile:
