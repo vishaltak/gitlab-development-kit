85d1529cbb9b8e9dcd645a1b63e6ad839a88583a
Nao Hashizume <nhashizume@gitlab.com>
2024-01-29 14:30:44 -0800

    Create a temporary file for runner .tool-versions

--- support/makefiles/Makefile.gitlab-runner.mk
+++ support/makefiles/Makefile.gitlab-runner.mk
@@ -1,5 +1,6 @@
 runner_url = https://gitlab.com/gitlab-org/gitlab-runner
-runner_tool_versions_file = ${runner_url}/-/raw/main/.tool-versions
+runner_tool_versions_remote_file = ${runner_url}/-/raw/main/.tool-versions
+runner_tool_versions_local_file = tmp/.tool-versions-runner
 
 runner-setup: runner-asdf-install gitlab-runner-config.toml
 
@@ -7,12 +8,11 @@ runner-asdf-install:
 ifeq ($(asdf_opt_out),false)
 	@echo
 	@echo "${DIVIDER}"
-	@echo "Installing asdf tools from ${runner_tool_versions_file}"
+	@echo "Installing asdf tools from ${runner_tool_versions_remote_file}"
 	@echo "${DIVIDER}"
-	$(Q)curl --silent ${runner_tool_versions_file} > .tool-versions-runner
-	$(Q)ASDF_DEFAULT_TOOL_VERSIONS_FILENAME=".tool-versions-runner" asdf install
+	$(Q)curl --silent ${runner_tool_versions_remote_file} > ${runner_tool_versions_local_file}
+	$(Q)ASDF_DEFAULT_TOOL_VERSIONS_FILENAME="${runner_tool_versions_local_file}" asdf install
 	$(Q)asdf reshim
-	$(Q)rm -rf .tool-versions-runner
 else
 	@true
 endif
